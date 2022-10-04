854e2b5e11f374d05bec83b99a1572c08617d34d
Chinchilla7 <regina.chen@stonybrook.edu>
2022-10-03 21:19:05 -0400

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,1 +1,18 @@
+#sudo apt-get install python3-dev default-libmysqlclient-dev
+#pip install pymysql
 
+from os import environ
+from sqlalchemy import create_engine
+import pandas as pd
+
+#file path containing credentials
+
+connection_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
+connection_string
+
+db = create_engine(connection_string)
+
+r_df = pd.read_csv('')
+r_df.to_sql('dummy_data', con=db, if_exists='replace')
+
+sql_query = """select * from dummy_data where gender = 'Male'"""
