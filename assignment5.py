dae46b524283bb0cab27bdd3ad8421d41906483d
Chinchilla7 <regina.chen@stonybrook.edu>
2022-10-06 14:52:32 -0400

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -6,10 +6,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import dotenv_values
 
+#path containing credentials
 config = dotenv_values(".env")
 
-#file path containing credentials
-
+#connect to mysql database using credentials
 
 connection_string = f'mysql+pymysql://{config["MYSQL_USER"]}:{config["MYSQL_PASSWORD"]}@{config["MYSQL_HOSTNAME"]}/{config["MYSQL_DATABASE"]}'
 connection_string
@@ -17,6 +17,7 @@ connection_string
 db = create_engine(connection_string)
 print (db.table_names())
 
+#creating a new table within dummy database called dummy_data containing data from iris csv 
 r_df = pd.read_csv('data/iris.csv')
 r_df.to_sql('dummy_data', con=db, if_exists='replace')
 
